65be52c5f42403a4ca2dd76cb3e93d76df59d823
Mikhail <mike.lyashenko@yandex.ru>
2019-02-01 13:24:35 +0300

    Add timestamp

--- order.pb.go
+++ order.pb.go
@@ -3,13 +3,13 @@
 
 package order
 
-import proto "github.com/golang/protobuf/proto"
-import fmt "fmt"
-import math "math"
-
 import (
-	context "golang.org/x/net/context"
+	context "context"
+	fmt "fmt"
+	proto "github.com/golang/protobuf/proto"
+	timestamp "github.com/golang/protobuf/ptypes/timestamp"
 	grpc "google.golang.org/grpc"
+	math "math"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -21,48 +21,49 @@ var _ = math.Inf
 // is compatible with the proto package it is being compiled against.
 // A compilation error at this line likely means your copy of the
 // proto package needs to be updated.
-const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
+const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
 type CreateOrderRequest struct {
-	Uuid                 string   `protobuf:"bytes,1,opt,name=uuid,proto3" json:"uuid,omitempty"`
-	CreatedAt            string   `protobuf:"bytes,2,opt,name=created_at,json=createdAt,proto3" json:"created_at,omitempty"`
-	Type                 int32    `protobuf:"varint,3,opt,name=type,proto3" json:"type,omitempty"`
-	VisitUuid            string   `protobuf:"bytes,4,opt,name=visit_uuid,json=visitUuid,proto3" json:"visit_uuid,omitempty"`
-	StreamCode           string   `protobuf:"bytes,5,opt,name=stream_code,json=streamCode,proto3" json:"stream_code,omitempty"`
-	OfferId              int32    `protobuf:"varint,6,opt,name=offer_id,json=offerId,proto3" json:"offer_id,omitempty"`
-	UserId               int32    `protobuf:"varint,7,opt,name=user_id,json=userId,proto3" json:"user_id,omitempty"`
-	ClientUuid           string   `protobuf:"bytes,8,opt,name=client_uuid,json=clientUuid,proto3" json:"client_uuid,omitempty"`
-	Status               int32    `protobuf:"varint,9,opt,name=status,proto3" json:"status,omitempty"`
-	Sum                  float32  `protobuf:"fixed32,10,opt,name=sum,proto3" json:"sum,omitempty"`
-	Payment              float32  `protobuf:"fixed32,11,opt,name=payment,proto3" json:"payment,omitempty"`
-	Currency             string   `protobuf:"bytes,12,opt,name=currency,proto3" json:"currency,omitempty"`
-	GeoCode              []string `protobuf:"bytes,13,rep,name=geo_code,json=geoCode,proto3" json:"geo_code,omitempty"`
-	Sub1                 string   `protobuf:"bytes,14,opt,name=sub1,proto3" json:"sub1,omitempty"`
-	Sub2                 string   `protobuf:"bytes,15,opt,name=sub2,proto3" json:"sub2,omitempty"`
-	Sub3                 string   `protobuf:"bytes,16,opt,name=sub3,proto3" json:"sub3,omitempty"`
-	Sub4                 string   `protobuf:"bytes,17,opt,name=sub4,proto3" json:"sub4,omitempty"`
-	Sub5                 string   `protobuf:"bytes,18,opt,name=sub5,proto3" json:"sub5,omitempty"`
-	ErrorReason          string   `protobuf:"bytes,19,opt,name=error_reason,json=errorReason,proto3" json:"error_reason,omitempty"`
-	Pills                int32    `protobuf:"varint,20,opt,name=pills,proto3" json:"pills,omitempty"`
-	XXX_NoUnkeyedLiteral struct{} `json:"-"`
-	XXX_unrecognized     []byte   `json:"-"`
-	XXX_sizecache        int32    `json:"-"`
+	Uuid                 string               `protobuf:"bytes,1,opt,name=uuid,proto3" json:"uuid,omitempty"`
+	CreatedAt            *timestamp.Timestamp `protobuf:"bytes,2,opt,name=created_at,json=createdAt,proto3" json:"created_at,omitempty"`
+	Type                 int32                `protobuf:"varint,3,opt,name=type,proto3" json:"type,omitempty"`
+	VisitUuid            string               `protobuf:"bytes,4,opt,name=visit_uuid,json=visitUuid,proto3" json:"visit_uuid,omitempty"`
+	StreamCode           string               `protobuf:"bytes,5,opt,name=stream_code,json=streamCode,proto3" json:"stream_code,omitempty"`
+	OfferId              int32                `protobuf:"varint,6,opt,name=offer_id,json=offerId,proto3" json:"offer_id,omitempty"`
+	UserId               int32                `protobuf:"varint,7,opt,name=user_id,json=userId,proto3" json:"user_id,omitempty"`
+	ClientUuid           string               `protobuf:"bytes,8,opt,name=client_uuid,json=clientUuid,proto3" json:"client_uuid,omitempty"`
+	Status               int32                `protobuf:"varint,9,opt,name=status,proto3" json:"status,omitempty"`
+	Sum                  float32              `protobuf:"fixed32,10,opt,name=sum,proto3" json:"sum,omitempty"`
+	Payment              float32              `protobuf:"fixed32,11,opt,name=payment,proto3" json:"payment,omitempty"`
+	Currency             string               `protobuf:"bytes,12,opt,name=currency,proto3" json:"currency,omitempty"`
+	GeoCode              []string             `protobuf:"bytes,13,rep,name=geo_code,json=geoCode,proto3" json:"geo_code,omitempty"`
+	Sub1                 string               `protobuf:"bytes,14,opt,name=sub1,proto3" json:"sub1,omitempty"`
+	Sub2                 string               `protobuf:"bytes,15,opt,name=sub2,proto3" json:"sub2,omitempty"`
+	Sub3                 string               `protobuf:"bytes,16,opt,name=sub3,proto3" json:"sub3,omitempty"`
+	Sub4                 string               `protobuf:"bytes,17,opt,name=sub4,proto3" json:"sub4,omitempty"`
+	Sub5                 string               `protobuf:"bytes,18,opt,name=sub5,proto3" json:"sub5,omitempty"`
+	ErrorReason          string               `protobuf:"bytes,19,opt,name=error_reason,json=errorReason,proto3" json:"error_reason,omitempty"`
+	Pills                int32                `protobuf:"varint,20,opt,name=pills,proto3" json:"pills,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}             `json:"-"`
+	XXX_unrecognized     []byte               `json:"-"`
+	XXX_sizecache        int32                `json:"-"`
 }
 
 func (m *CreateOrderRequest) Reset()         { *m = CreateOrderRequest{} }
 func (m *CreateOrderRequest) String() string { return proto.CompactTextString(m) }
 func (*CreateOrderRequest) ProtoMessage()    {}
 func (*CreateOrderRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_order_a3e18a5d49ad4ce5, []int{0}
+	return fileDescriptor_cd01338c35d87077, []int{0}
 }
+
 func (m *CreateOrderRequest) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_CreateOrderRequest.Unmarshal(m, b)
 }
 func (m *CreateOrderRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_CreateOrderRequest.Marshal(b, m, deterministic)
 }
-func (dst *CreateOrderRequest) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_CreateOrderRequest.Merge(dst, src)
+func (m *CreateOrderRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_CreateOrderRequest.Merge(m, src)
 }
 func (m *CreateOrderRequest) XXX_Size() int {
 	return xxx_messageInfo_CreateOrderRequest.Size(m)
@@ -80,11 +81,11 @@ func (m *CreateOrderRequest) GetUuid() string {
 	return ""
 }
 
-func (m *CreateOrderRequest) GetCreatedAt() string {
+func (m *CreateOrderRequest) GetCreatedAt() *timestamp.Timestamp {
 	if m != nil {
 		return m.CreatedAt
 	}
-	return ""
+	return nil
 }
 
 func (m *CreateOrderRequest) GetType() int32 {
@@ -214,45 +215,46 @@ func (m *CreateOrderRequest) GetPills() int32 {
 }
 
 type Order struct {
-	Uuid                 string   `protobuf:"bytes,1,opt,name=uuid,proto3" json:"uuid,omitempty"`
-	CreatedAt            string   `protobuf:"bytes,2,opt,name=created_at,json=createdAt,proto3" json:"created_at,omitempty"`
-	Type                 int32    `protobuf:"varint,3,opt,name=type,proto3" json:"type,omitempty"`
-	VisitUuid            string   `protobuf:"bytes,4,opt,name=visit_uuid,json=visitUuid,proto3" json:"visit_uuid,omitempty"`
-	StreamCode           string   `protobuf:"bytes,5,opt,name=stream_code,json=streamCode,proto3" json:"stream_code,omitempty"`
-	OfferId              int32    `protobuf:"varint,6,opt,name=offer_id,json=offerId,proto3" json:"offer_id,omitempty"`
-	UserId               int32    `protobuf:"varint,7,opt,name=user_id,json=userId,proto3" json:"user_id,omitempty"`
-	ClientUuid           string   `protobuf:"bytes,8,opt,name=client_uuid,json=clientUuid,proto3" json:"client_uuid,omitempty"`
-	Status               int32    `protobuf:"varint,9,opt,name=status,proto3" json:"status,omitempty"`
-	Sum                  float32  `protobuf:"fixed32,10,opt,name=sum,proto3" json:"sum,omitempty"`
-	Payment              float32  `protobuf:"fixed32,11,opt,name=payment,proto3" json:"payment,omitempty"`
-	Currency             string   `protobuf:"bytes,12,opt,name=currency,proto3" json:"currency,omitempty"`
-	GeoCode              []string `protobuf:"bytes,13,rep,name=geo_code,json=geoCode,proto3" json:"geo_code,omitempty"`
-	Sub1                 string   `protobuf:"bytes,14,opt,name=sub1,proto3" json:"sub1,omitempty"`
-	Sub2                 string   `protobuf:"bytes,15,opt,name=sub2,proto3" json:"sub2,omitempty"`
-	Sub3                 string   `protobuf:"bytes,16,opt,name=sub3,proto3" json:"sub3,omitempty"`
-	Sub4                 string   `protobuf:"bytes,17,opt,name=sub4,proto3" json:"sub4,omitempty"`
-	Sub5                 string   `protobuf:"bytes,18,opt,name=sub5,proto3" json:"sub5,omitempty"`
-	ErrorReason          string   `protobuf:"bytes,19,opt,name=error_reason,json=errorReason,proto3" json:"error_reason,omitempty"`
-	Pills                int32    `protobuf:"varint,20,opt,name=pills,proto3" json:"pills,omitempty"`
-	XXX_NoUnkeyedLiteral struct{} `json:"-"`
-	XXX_unrecognized     []byte   `json:"-"`
-	XXX_sizecache        int32    `json:"-"`
+	Uuid                 string               `protobuf:"bytes,1,opt,name=uuid,proto3" json:"uuid,omitempty"`
+	CreatedAt            *timestamp.Timestamp `protobuf:"bytes,2,opt,name=created_at,json=createdAt,proto3" json:"created_at,omitempty"`
+	Type                 int32                `protobuf:"varint,3,opt,name=type,proto3" json:"type,omitempty"`
+	VisitUuid            string               `protobuf:"bytes,4,opt,name=visit_uuid,json=visitUuid,proto3" json:"visit_uuid,omitempty"`
+	StreamCode           string               `protobuf:"bytes,5,opt,name=stream_code,json=streamCode,proto3" json:"stream_code,omitempty"`
+	OfferId              int32                `protobuf:"varint,6,opt,name=offer_id,json=offerId,proto3" json:"offer_id,omitempty"`
+	UserId               int32                `protobuf:"varint,7,opt,name=user_id,json=userId,proto3" json:"user_id,omitempty"`
+	ClientUuid           string               `protobuf:"bytes,8,opt,name=client_uuid,json=clientUuid,proto3" json:"client_uuid,omitempty"`
+	Status               int32                `protobuf:"varint,9,opt,name=status,proto3" json:"status,omitempty"`
+	Sum                  float32              `protobuf:"fixed32,10,opt,name=sum,proto3" json:"sum,omitempty"`
+	Payment              float32              `protobuf:"fixed32,11,opt,name=payment,proto3" json:"payment,omitempty"`
+	Currency             string               `protobuf:"bytes,12,opt,name=currency,proto3" json:"currency,omitempty"`
+	GeoCode              []string             `protobuf:"bytes,13,rep,name=geo_code,json=geoCode,proto3" json:"geo_code,omitempty"`
+	Sub1                 string               `protobuf:"bytes,14,opt,name=sub1,proto3" json:"sub1,omitempty"`
+	Sub2                 string               `protobuf:"bytes,15,opt,name=sub2,proto3" json:"sub2,omitempty"`
+	Sub3                 string               `protobuf:"bytes,16,opt,name=sub3,proto3" json:"sub3,omitempty"`
+	Sub4                 string               `protobuf:"bytes,17,opt,name=sub4,proto3" json:"sub4,omitempty"`
+	Sub5                 string               `protobuf:"bytes,18,opt,name=sub5,proto3" json:"sub5,omitempty"`
+	ErrorReason          string               `protobuf:"bytes,19,opt,name=error_reason,json=errorReason,proto3" json:"error_reason,omitempty"`
+	Pills                int32                `protobuf:"varint,20,opt,name=pills,proto3" json:"pills,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}             `json:"-"`
+	XXX_unrecognized     []byte               `json:"-"`
+	XXX_sizecache        int32                `json:"-"`
 }
 
 func (m *Order) Reset()         { *m = Order{} }
 func (m *Order) String() string { return proto.CompactTextString(m) }
 func (*Order) ProtoMessage()    {}
 func (*Order) Descriptor() ([]byte, []int) {
-	return fileDescriptor_order_a3e18a5d49ad4ce5, []int{1}
+	return fileDescriptor_cd01338c35d87077, []int{1}
 }
+
 func (m *Order) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Order.Unmarshal(m, b)
 }
 func (m *Order) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_Order.Marshal(b, m, deterministic)
 }
-func (dst *Order) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Order.Merge(dst, src)
+func (m *Order) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Order.Merge(m, src)
 }
 func (m *Order) XXX_Size() int {
 	return xxx_messageInfo_Order.Size(m)
@@ -270,11 +272,11 @@ func (m *Order) GetUuid() string {
 	return ""
 }
 
-func (m *Order) GetCreatedAt() string {
+func (m *Order) GetCreatedAt() *timestamp.Timestamp {
 	if m != nil {
 		return m.CreatedAt
 	}
-	return ""
+	return nil
 }
 
 func (m *Order) GetType() int32 {
@@ -408,6 +410,39 @@ func init() {
 	proto.RegisterType((*Order)(nil), "order.Order")
 }
 
+func init() { proto.RegisterFile("order.proto", fileDescriptor_cd01338c35d87077) }
+
+var fileDescriptor_cd01338c35d87077 = []byte{
+	// 428 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xec, 0x94, 0xcf, 0x6a, 0xdb, 0x40,
+	0x10, 0xc6, 0x51, 0x2c, 0x59, 0xd6, 0xc8, 0x6d, 0xd3, 0x6d, 0x68, 0x27, 0x86, 0x12, 0x35, 0x27,
+	0x9d, 0x14, 0x6a, 0x27, 0x85, 0x1e, 0x4b, 0xa0, 0x90, 0x53, 0x41, 0x6d, 0xcf, 0x42, 0x96, 0xc6,
+	0x46, 0x60, 0x69, 0xd5, 0xfd, 0x13, 0xf0, 0x93, 0xf4, 0x05, 0xfa, 0xa0, 0x45, 0xb3, 0xb2, 0x08,
+	0xe4, 0x15, 0x72, 0x9b, 0xef, 0x37, 0xab, 0xf9, 0xb4, 0xcb, 0xc7, 0x40, 0x2c, 0x55, 0x4d, 0x2a,
+	0xeb, 0x95, 0x34, 0x52, 0x04, 0x2c, 0x56, 0x57, 0x7b, 0x29, 0xf7, 0x07, 0xba, 0x61, 0xb8, 0xb5,
+	0xbb, 0x1b, 0xd3, 0xb4, 0xa4, 0x4d, 0xd9, 0xf6, 0xee, 0xdc, 0xf5, 0x3f, 0x1f, 0xc4, 0xbd, 0xa2,
+	0xd2, 0xd0, 0x8f, 0xe1, 0x83, 0x9c, 0xfe, 0x58, 0xd2, 0x46, 0x08, 0xf0, 0xad, 0x6d, 0x6a, 0xf4,
+	0x12, 0x2f, 0x8d, 0x72, 0xae, 0xc5, 0x57, 0x80, 0x8a, 0x4f, 0xd6, 0x45, 0x69, 0xf0, 0x2c, 0xf1,
+	0xd2, 0x78, 0xbd, 0xca, 0x9c, 0x41, 0x76, 0x32, 0xc8, 0x7e, 0x9d, 0x0c, 0xf2, 0x68, 0x3c, 0xfd,
+	0x8d, 0xc7, 0x99, 0x63, 0x4f, 0x38, 0x4b, 0xbc, 0x34, 0xc8, 0xb9, 0x16, 0x1f, 0x01, 0x1e, 0x1b,
+	0xdd, 0x98, 0x82, 0x8d, 0x7c, 0x36, 0x8a, 0x98, 0xfc, 0x1e, 0xdc, 0xae, 0x20, 0xd6, 0x46, 0x51,
+	0xd9, 0x16, 0x95, 0xac, 0x09, 0x03, 0xee, 0x83, 0x43, 0xf7, 0xb2, 0x26, 0x71, 0x09, 0x0b, 0xb9,
+	0xdb, 0x91, 0x2a, 0x9a, 0x1a, 0xe7, 0x3c, 0x37, 0x64, 0xfd, 0x50, 0x8b, 0x0f, 0x10, 0x5a, 0xed,
+	0x3a, 0x21, 0x77, 0xe6, 0x83, 0x7c, 0xe0, 0xa1, 0xd5, 0xa1, 0xa1, 0x6e, 0x34, 0x5d, 0xb8, 0xa1,
+	0x0e, 0xb1, 0xeb, 0x7b, 0x98, 0x6b, 0x53, 0x1a, 0xab, 0x31, 0x72, 0x1f, 0x3a, 0x25, 0xce, 0x61,
+	0xa6, 0x6d, 0x8b, 0x90, 0x78, 0xe9, 0x59, 0x3e, 0x94, 0x02, 0x21, 0xec, 0xcb, 0x63, 0x4b, 0x9d,
+	0xc1, 0x98, 0xe9, 0x49, 0x8a, 0x15, 0x2c, 0x2a, 0xab, 0x14, 0x75, 0xd5, 0x11, 0x97, 0xec, 0x30,
+	0xe9, 0xe1, 0xa7, 0xf7, 0x24, 0xdd, 0x95, 0x5e, 0x25, 0xb3, 0x34, 0xca, 0xc3, 0x3d, 0x49, 0xbe,
+	0x8f, 0x00, 0x5f, 0xdb, 0xed, 0x67, 0x7c, 0xed, 0x9e, 0x7c, 0xa8, 0x47, 0xb6, 0xc6, 0x37, 0x13,
+	0x5b, 0x8f, 0x6c, 0x83, 0xe7, 0x13, 0xdb, 0x8c, 0xec, 0x16, 0xdf, 0x4e, 0xec, 0x76, 0x64, 0x77,
+	0x28, 0x26, 0x76, 0x27, 0x3e, 0xc1, 0x92, 0x94, 0x92, 0xaa, 0x50, 0x54, 0x6a, 0xd9, 0xe1, 0x3b,
+	0xee, 0xc5, 0xcc, 0x72, 0x46, 0xe2, 0x02, 0x82, 0xbe, 0x39, 0x1c, 0x34, 0x5e, 0xf0, 0x03, 0x38,
+	0x71, 0xfd, 0xd7, 0x87, 0x80, 0x03, 0xf2, 0x92, 0x8c, 0x97, 0x64, 0x3c, 0x4d, 0xc6, 0xfa, 0x3b,
+	0x2c, 0x39, 0x18, 0x3f, 0x49, 0x3d, 0x36, 0x15, 0x89, 0x2f, 0x10, 0x3f, 0xd9, 0x27, 0xe2, 0x32,
+	0x73, 0x5b, 0xe9, 0xf9, 0x8e, 0x59, 0x2d, 0xc7, 0x16, 0xc3, 0xed, 0x9c, 0x73, 0xb2, 0xf9, 0x1f,
+	0x00, 0x00, 0xff, 0xff, 0x81, 0x1e, 0x89, 0xd8, 0xc6, 0x04, 0x00, 0x00,
+}
+
 // Reference imports to suppress errors if they are not otherwise used.
 var _ context.Context
 var _ grpc.ClientConn
@@ -478,35 +513,4 @@ var _OrderService_serviceDesc = grpc.ServiceDesc{
 	},
 	Streams:  []grpc.StreamDesc{},
 	Metadata: "order.proto",
-}
-
-func init() { proto.RegisterFile("order.proto", fileDescriptor_order_a3e18a5d49ad4ce5) }
-
-var fileDescriptor_order_a3e18a5d49ad4ce5 = []byte{
-	// 388 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xec, 0x94, 0xcd, 0x6a, 0xd5, 0x50,
-	0x10, 0xc7, 0x49, 0xf3, 0x75, 0x33, 0x89, 0x5a, 0xc7, 0xa2, 0xd3, 0x82, 0x18, 0xbb, 0xca, 0xaa,
-	0xe0, 0xbd, 0xad, 0x7b, 0x29, 0x08, 0x5d, 0x09, 0x11, 0xd7, 0x21, 0x4d, 0xa6, 0x25, 0x70, 0x6f,
-	0x4e, 0x3c, 0x1f, 0x85, 0xfb, 0x06, 0xbe, 0xb3, 0x1b, 0xc9, 0x9c, 0x18, 0x04, 0x1f, 0x41, 0x77,
-	0xf3, 0xff, 0xfd, 0x33, 0x33, 0x39, 0x67, 0x0e, 0x03, 0xb9, 0xd2, 0x3d, 0xeb, 0xab, 0x49, 0x2b,
-	0xab, 0x30, 0x16, 0x71, 0xf9, 0x23, 0x02, 0xbc, 0xd5, 0xdc, 0x5a, 0xfe, 0x32, 0xeb, 0x9a, 0xbf,
-	0x3b, 0x36, 0x16, 0x11, 0x22, 0xe7, 0x86, 0x9e, 0x82, 0x32, 0xa8, 0xb2, 0x5a, 0x62, 0x7c, 0x0b,
-	0xd0, 0xc9, 0x97, 0x7d, 0xd3, 0x5a, 0x3a, 0x11, 0x27, 0x5b, 0xc8, 0x27, 0x49, 0xb1, 0xc7, 0x89,
-	0x29, 0x2c, 0x83, 0x2a, 0xae, 0x25, 0x9e, 0x53, 0x9e, 0x06, 0x33, 0xd8, 0x46, 0x8a, 0x45, 0x3e,
-	0x45, 0xc8, 0xb7, 0xb9, 0xe2, 0x3b, 0xc8, 0x8d, 0xd5, 0xdc, 0x1e, 0x9a, 0x4e, 0xf5, 0x4c, 0xb1,
-	0xf8, 0xe0, 0xd1, 0xad, 0xea, 0x19, 0xcf, 0x61, 0xa3, 0x1e, 0x1e, 0x58, 0x37, 0x43, 0x4f, 0x89,
-	0xd4, 0x4d, 0x45, 0xdf, 0xf5, 0xf8, 0x06, 0x52, 0x67, 0xbc, 0x93, 0x8a, 0x93, 0xcc, 0xf2, 0x4e,
-	0x8a, 0x76, 0xfb, 0x81, 0xc7, 0xa5, 0xe9, 0xc6, 0x17, 0xf5, 0x48, 0xba, 0xbe, 0x86, 0xc4, 0xd8,
-	0xd6, 0x3a, 0x43, 0x99, 0x4f, 0xf4, 0x0a, 0x4f, 0x21, 0x34, 0xee, 0x40, 0x50, 0x06, 0xd5, 0x49,
-	0x3d, 0x87, 0x48, 0x90, 0x4e, 0xed, 0xf1, 0xc0, 0xa3, 0xa5, 0x5c, 0xe8, 0x6f, 0x89, 0x17, 0xb0,
-	0xe9, 0x9c, 0xd6, 0x3c, 0x76, 0x47, 0x2a, 0xa4, 0xc3, 0xaa, 0xe7, 0x9f, 0x7e, 0x64, 0xe5, 0x8f,
-	0xf4, 0xac, 0x0c, 0xab, 0xac, 0x4e, 0x1f, 0x59, 0xc9, 0x79, 0x10, 0x22, 0xe3, 0xee, 0x3f, 0xd0,
-	0x73, 0x7f, 0xad, 0x73, 0xbc, 0xb0, 0x2d, 0xbd, 0x58, 0xd9, 0x76, 0x61, 0x3b, 0x3a, 0x5d, 0xd9,
-	0x6e, 0x61, 0xd7, 0xf4, 0x72, 0x65, 0xd7, 0x0b, 0xbb, 0x21, 0x5c, 0xd9, 0x0d, 0xbe, 0x87, 0x82,
-	0xb5, 0x56, 0xba, 0xd1, 0xdc, 0x1a, 0x35, 0xd2, 0x2b, 0xf1, 0x72, 0x61, 0xb5, 0x20, 0x3c, 0x83,
-	0x78, 0x1a, 0xf6, 0x7b, 0x43, 0x67, 0x72, 0x01, 0x5e, 0x5c, 0xfe, 0x0c, 0x21, 0x96, 0x47, 0xf0,
-	0x7f, 0xfa, 0xff, 0xde, 0xf4, 0xb7, 0x9f, 0xa1, 0x90, 0xe1, 0x7f, 0x65, 0xfd, 0x34, 0x74, 0x8c,
-	0x1f, 0x21, 0xff, 0x63, 0x2f, 0xe0, 0xf9, 0x95, 0x5f, 0x1e, 0x7f, 0xef, 0x8a, 0x8b, 0x62, 0xb1,
-	0x04, 0xde, 0x27, 0xb2, 0x5e, 0x76, 0xbf, 0x02, 0x00, 0x00, 0xff, 0xff, 0x0b, 0x77, 0x79, 0xfc,
-	0x6d, 0x04, 0x00, 0x00,
 }
